4cc13945b642518a698bc511fe784daa48633db2
staceymosier <mosierdesign@gmail.com>
2019-08-04 20:47:19 -0400

    Update static site

--- dist/src/js/script.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  "use strict";
-  var anchors = document.querySelectorAll("a");
-  for (let anchor=0; anchor<anchors.length; anchor++) {
-    anchors[anchor].setAttribute("target", "_blank");
-  }
-
-})();
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,14 @@
-(function() {
 
-  "use strict";
-  var anchors = document.querySelectorAll("a");
-  for (let anchor=0; anchor<anchors.length; anchor++) {
-    anchors[anchor].setAttribute("target", "_blank");
-  }
+const tags = ['JavaScript', 'SCSS', 'Responsive Web Design', 'SVG', 'PHP', 'Content Management', 'User Experience', 'Project Management', 'Integration',
+ 'Drupal 8', 'Data Management', 'MySQL', 'User Workflows', 'SEO', 'Accessibility', 'User Training',
+ 'VSCode', 'Sublime Text', 'ESLint', 'Gulp', 'PHP Code Sniifer']
 
-})();
+const tagsParent = document.querySelector('#tags');
+if (tagsParent) {
+  tags.forEach(function (tag) {
+    let span = document.createElement('span');
+    span.className = 'tag';
+    span.innerHTML = tag;
+    tagsParent.appendChild(span);
+  });
+};
